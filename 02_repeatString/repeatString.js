7216e62d973cd956455d24b5f746e771acb8c484
Col <133714102+awakeallhours@users.noreply.github.com>
2024-12-09 07:55:04 +0000

    tired

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    for (let i = 0; i < num; i++) {
+        process.stdout.write(num);
+    }
 };
+
+repeatString("test",3);
 
 // Do not edit below this line
 module.exports = repeatString;
