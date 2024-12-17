f2188b04365978b1c07833d307b54a78a31dd1f3
Col <133714102+awakeallhours@users.noreply.github.com>
2024-12-17 00:04:21 +0000

    shopping list setup

--- domManipulation/domManipulation.js
+++ domManipulation/domManipulation.js
@@ -18,21 +18,34 @@ h3.textContent = "I'm a blue h3!";
 h3.setAttribute("style", "color: blue;")
 container.appendChild(h3)
 
-//i dont think this div is being added, try it later when i have had a sleep
 const practice = document.createElement("div");
 practice.classList.add("domPractice");
 practice.textContent = "I'm a second div";
-practice.setAttribute("style", "border: solid black; backgroundColour: pink;");
-const h1 = document.createElement("div");
-h1 = document.add("h1");
+container.appendChild(practice)
+practice.setAttribute("style", "border: solid black; background: pink;");
+const h1 = document.createElement("h1");
+h1.classList.add("h1");
 h1.textContent = "I'm a div";
 const para2 = document.createElement("p");
 para2.classList.add("paragraph");
 para2.textContent = "ME TOO!";
-//practice.appendChild(h1,para2)
-container.appendChild(practice, h1, para2)
-/*
-a <div> with a black border and pink background color with the following elements inside of it:
-another <h1> that says “I’m in a div”
-a <p> that says “ME TOO!”
-Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.*/ 
+practice.appendChild(h1)
+practice.appendChild(para2)
+container.appendChild(practice)
+
+
+btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+  });
+
+  // buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
